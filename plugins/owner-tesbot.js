341c10960a8bac4d1d8cbb0d48199306b584b151
ssethc <pribrezaa@yahoo.com>
2022-11-13 22:29:04 +0700

    first commit

---
+++ plugins/owner-tesbot.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch'
+import fs from 'fs';
+import crypto from 'crypto'
+let handler = async (m, { conn, text, usedPrefix }) => {
+    const ultah = new Date('November 4 2022 23:59:59')
+    const sekarat = new Date().getTime() 
+    const Kurang = ultah - sekarat
+    const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
+    const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
+    const detek = Math.floor( Kurang % (1000 * 60) / 1000)
+    let sewa = `*🎏 Bot Aktif Kak*
+
+${wm}
+${botdate}
+`
+        let img1 = fs.readFileSync('./thumbnail.jpg');
+        let img2 = fs.readFileSync('./media/bawah.png');
+    return conn.sendButton(m.chat, hiasan, sewa, await (await fetch(`https://telegra.ph/file/bb235032497c153609d7e.jpg`)).buffer(), [["🗳️MENU", usedPrefix + "menu"], ["🔭DONASI", usedPrefix + "donasi"]], m, {
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: '',
+                        mediaType: 2,
+                        description: 'anu',
+                        title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+                        body: `Subscribe YT My Bestie`,                                       previewType: 0,
+                        thumbnail: await (await fetch(`https://telegra.ph/file/bb235032497c153609d7e.jpg`)).buffer(),
+                        sourceUrl: 'https://youtube.com/channel/UClo33d5kGNdUXOqwDb1UCNA'
+            }
+        }
+    })
+}
+handler.help = ['tesbot']
+handler.tags = ['owner']
+handler.command = ['tesbot']
+handler.owner = true
+export default handler
